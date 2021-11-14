27804945f590c3844af9c1b89c68c6effc80a42f
Vlad Yacishin <d_m_n04@mail.ru>
2021-11-14 12:09:41 +0300

    Fix timer timezone, now only +3

--- resources/js/countdown-init.js
+++ resources/js/countdown-init.js
@@ -1,5 +1,10 @@
+import moment from "moment-timezone";
+
 $( '.sd-countdown' ).each( function eachCountdown() {
     let dateEnd = $( this ).attr( 'data-end' );
+    moment.tz.setDefault( "Europe/Moscow" );
+    dateEnd = moment( dateEnd ).toDate();
+
     $( this ).countdown( dateEnd, function( event ) {
         let totalHours = event.offset.totalDays * 24 + event.offset.hours;
         $( this ).html( event.strftime( '<div><span>' + totalHours + ':%M:%S</span></div>' ) );
