2c8e1b7d4b20aaace37afaef58e8ce5a01acc3d6
Vlad Yacishin <d_m_n04@mail.ru>
2021-11-26 15:21:32 +0300

    Change to correct route

--- resources/views/pages/resources/show.blade.php
+++ resources/views/pages/resources/show.blade.php
@@ -39,7 +39,7 @@
 
                 @foreach($single->recipes as $single)
                     <div class="col-4 pb-10">
-                        <a href="{{ route( 'resources.show', [ 'id' => $single->id ] ) }}">
+                        <a href="{{ route( 'recipes.show', [ 'id' => $single->id ] ) }}">
                             <img width="30" src="{{asset('storage') . '/' . $single->img}}" alt="{{$single->name}}">
                             {{$single->name}}
                         </a>
