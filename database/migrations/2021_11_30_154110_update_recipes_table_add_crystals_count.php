d04e9925c1be042234420a4c0822964e86829912
Vlad Yacishin <d_m_n04@mail.ru>
2021-11-30 21:53:15 +0300

    Add crystal count and reformate a bit

---
+++ database/migrations/2021_11_30_154110_update_recipes_table_add_crystals_count.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateRecipesTableAddCrystalsCount extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::table( 'recipes', function ( Blueprint $table ) {
+            $table->bigInteger( 'crystals_count' )->nullable();
+        } );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::table( 'recipes', function ( Blueprint $table ) {
+            $table->dropColumn( 'crystals_count' );
+        } );
+    }
+}
