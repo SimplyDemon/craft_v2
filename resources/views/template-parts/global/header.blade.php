2364aa2ef07d01e82d092f9dd9677c44dbd09725
Vlad <d_m_n04@mail.ru>
2022-01-16 15:41:41 +0300

    Ya metrica update

--- resources/views/template-parts/global/header.blade.php
+++ resources/views/template-parts/global/header.blade.php
@@ -75,27 +75,17 @@ $_SESSION['isAnimationWasShowed'] = true;
 
 @if (env( 'APP_ENV' ) === 'prod')
     <!-- Yandex.Metrika counter -->
-    <script type="text/javascript">
-        (function( m, e, t, r, i, k, a ) {
+    <script type="text/javascript"> (function( m, e, t, r, i, k, a ) {
             m[ i ]   = m[ i ] || function() {
                 (m[ i ].a = m[ i ].a || []).push( arguments )
             };
             m[ i ].l = 1 * new Date();
             k = e.createElement( t ), a = e.getElementsByTagName( t )[ 0 ], k.async = 1, k.src = r, a.parentNode.insertBefore( k, a )
-        })
-        ( window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym" );
-
-        ym( 86545727, "init", {
-            clickmap: true,
-            trackLinks: true,
-            accurateTrackBounce: true,
-            webvisor: true
-        } );
-    </script>
+        })( window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym" );
+        ym( 86545727, "init", { clickmap: true, trackLinks: true, accurateTrackBounce: true } ); </script>
     <noscript>
-        <div><img src="https://mc.yandex.ru/watch/86545727" style="position:absolute; left:-9999px;" alt="ya" /></div>
-    </noscript>
-    <!-- /Yandex.Metrika counter -->
+        <div><img src="https://mc.yandex.ru/watch/86545727" style="position:absolute; left:-9999px;" alt="" /></div>
+    </noscript> <!-- /Yandex.Metrika counter -->
 @endif
 
 @if($isEnableAnimation)
