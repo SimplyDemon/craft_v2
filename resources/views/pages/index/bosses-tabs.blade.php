49d8518b9684244d2dea5742cfc204e98e8f4a1a
Vlad <d_m_n04@mail.ru>
2022-09-09 17:12:34 +0300

    Update h2

--- resources/views/pages/index/bosses-tabs.blade.php
+++ resources/views/pages/index/bosses-tabs.blade.php
@@ -4,16 +4,17 @@
             <a class="nav-link active color-main-1-important" href="#{{$server}}-subclass" role="tab" data-toggle="tab">Сабкласс РБ</a>
         </li>
         @if($bosses['epic']->isNotEmpty())
-        <li class="nav-item">
-            <a class="nav-link color-main-1-important" href="#{{$server}}-epic" role="tab" data-toggle="tab">Эпик РБ</a>
-        </li>
+            <li class="nav-item">
+                <a class="nav-link color-main-1-important" href="#{{$server}}-epic" role="tab" data-toggle="tab">Эпик
+                    РБ</a>
+            </li>
         @endif
     </ul>
     <div class="tab-content">
         <div role="tabpanel" class="tab-pane fade show active" id="{{$server}}-subclass">
             <div class="nk-gap-1"></div>
 
-            <h2 class="nk-title h1">Subclass RB respawn {{$server}}</h2>
+            <h2 class="nk-title h1">Респ сабкласс РБ {{$server}}</h2>
             <div class="nk-gap-2"></div>
             <div class="row no-gutters">
 
@@ -28,24 +29,25 @@
             <div class="nk-gap-1"></div>
         </div>
         @if($bosses['epic']->isNotEmpty())
-        <div role="tabpanel" class="tab-pane fade" id="{{$server}}-epic">
-            <div class="nk-gap-1"></div>
+            <div role="tabpanel" class="tab-pane fade" id="{{$server}}-epic">
+                <div class="nk-gap-1"></div>
 
-            <h2 class="nk-title h1">Epic RB respawn {{$server}}</h2>
-            <p class="lead">
-                Внимательно следите за игровым чатом во время респа РБ, в нём анонсируется, что РБ респнется в течении
-                <span class="color-main-1-important">одного</span> или
-                <span class="color-main-1-important">двух</span> часов.
-            </p>
-            <div class="nk-gap-2"></div>
-            <div class="row no-gutters">
+                <h2 class="nk-title h1">Респ эпик РБ {{$server}}</h2>
+                <p class="lead">
+                    Внимательно следите за игровым чатом во время респа РБ, в нём анонсируется, что РБ респнется в
+                    течении
+                    <span class="color-main-1-important">одного</span> или
+                    <span class="color-main-1-important">двух</span> часов.
+                </p>
+                <div class="nk-gap-2"></div>
+                <div class="row no-gutters">
 
-                @foreach($bosses['epic']  as $boss)
-                    @include('pages.index.boss-timer', [
-                         'boss' => $boss,
-                         'colNumber' => 4,
+                    @foreach($bosses['epic']  as $boss)
+                        @include('pages.index.boss-timer', [
+                             'boss' => $boss,
+                             'colNumber' => 4,
 
-                     ])
+                         ])
                 @endforeach
             </div>
 
