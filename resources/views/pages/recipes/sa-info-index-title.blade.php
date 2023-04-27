88ca61ad935e6a4b0caf40ecc4319ef83349b011
Vlad <d_m_n04@mail.ru>
2023-04-27 10:03:32 +0300

    Prettify a bit

--- resources/views/pages/recipes/sa-info-index-title.blade.php
+++ resources/views/pages/recipes/sa-info-index-title.blade.php
@@ -1,4 +1,4 @@
-<div class='row'>
+<div class='recipes-index_sa-level row'>
     <div class='col-12'>
         <p>Уровень {{$saLvl}}</p>
     </div>
@@ -6,19 +6,19 @@
 <div class='row'>
     <div class='col-4'>
         <p>
-            <img width='20' src='{{$saRedImg}}' alt='SA Red'><i
+            <img class='recipes-index_sa-img' width='20' src='{{$saRedImg}}' alt='SA Red'><i
                 class='color-main-1-important'>{{$saRedName}}</i><br> {{$saRedDescription}}
         </p>
     </div>
     <div class='col-4'>
         <p>
-            <img width='20' src='{{$saGreenImg}}' alt='SA Green'><i
+            <img class='recipes-index_sa-img' width='20' src='{{$saGreenImg}}' alt='SA Green'><i
                 class='color-main-1-important'>{{$saGreenName}}</i><br>{{$saGreenDescription}}
         </p>
     </div>
     <div class='col-4'>
         <p>
-            <img width='20' src='{{$saBlueImg}}' alt='SA Blue'><i
+            <img class='recipes-index_sa-img' width='20' src='{{$saBlueImg}}' alt='SA Blue'><i
                 class='color-main-1-important'>{{$saBlueName}}</i><br>{{$saBlueDescription}}
         </p>
     </div>
